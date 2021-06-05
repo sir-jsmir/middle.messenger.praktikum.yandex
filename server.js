0d1bd92d997ebfda8916b03fe294c76587360ec5
sir-jsmir <84675819+sir-jsmir@users.noreply.github.com>
2021-06-05 19:19:22 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import path from 'path';
 
+const __dirname = path.resolve();
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('./dist'));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(PORT, function () {
     console.log(`App listening on port ${PORT}!`);

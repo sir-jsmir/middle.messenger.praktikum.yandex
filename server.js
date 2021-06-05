b9cf248ac2896beafbc3879c5bf581feb7a7d308
Jhon Smir <sir.jsmir@yandex.ru>
2021-06-05 17:14:53 +0500

    uploading a project

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('./dist'));
 
 app.listen(PORT, function () {
     console.log(`App listening on port ${PORT}!`);

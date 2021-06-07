7e002dffe0e2255ee00e45d7e0b67f9115c0cd93
Jhon Smir <sir.jsmir@yandex.ru>
2021-06-07 11:34:57 +0500

    Code review

--- src/pages/signIn/index.js
+++ src/pages/signIn/index.js
@@ -1,12 +1,11 @@
 import { getFormData } from "../../utils/getFormData";
 
 window.onload = () => {
-    const form = document.querySelector('.form');
-    form?.addEventListener('submit', (e) => {
+    const form = document.querySelector(".form");
+    form?.addEventListener("submit", (e) => {
         e.preventDefault();
         const result = getFormData(form);
-        console.log(result)
-        window.location.href = '../home/index.html';
+        console.log(result);
+        window.location.href = "../home/index.html";
     })
 };
-
--- src/utils/getFormData.js
+++ src/utils/getFormData.js
@@ -1,21 +1,18 @@
 export const getFormData = (form) => {
-    let result = {}
-    if (form) {
-        const elem = form.elements;
-        for (let i = 0; i < elem.length; i++) {
-            switch (elem[i].type) {
-                case 'submit':
-                    break;
-                case 'radio':
-                    break;
-                case 'checkbox':
-                    break;
-                default:
-                    result[elem[i].name] = elem[i].value;
-            }
+    let result = {};
+    if (!form) result;
+    const elem = form.elements;
+    for (let i = 0; i < elem.length; i++) {
+        switch (elem[i].type) {
+            case "submit":
+                break;
+            case "radio":
+                break;
+            case "checkbox":
+                break;
+            default:
+                result[elem[i].name] = elem[i].value;
         }
-    }
-    return result
+    };
+    return result;
 }
-
-
